8d49950a94dd80d8921ea011e42026f504f9024b
Vikash Chaurasiya <120702683+provikash@users.noreply.github.com>
2026-02-05 19:55:44 +0000

    init

---
+++ backend/users/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+  email=models.EmailField(unique=True)
+  username=models.CharField(max_length=150, unique=True)
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS=['email']
