53b471c1477d4cf1f0482ae37fe86705f3127d6c
vachaspatimishraa <vachaspatimishraa@gmail.com>
2026-03-11 04:35:40 +0530

    frontend_auth

--- prepmate_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ prepmate_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_darwin
+import google_sign_in_ios
+import path_provider_foundation
+import shared_preferences_foundation
+import smart_auth
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
