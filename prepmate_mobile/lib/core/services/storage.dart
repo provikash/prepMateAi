4a8b223c6477e49022fdc9ff59e9aab7755a69e3
vachaspatimishraa <vachaspatimishraa@gmail.com>
2026-04-01 21:52:56 +0530

    updated

---
+++ prepmate_mobile/lib/core/services/storage.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageSerivce {
+  static const _storage = FlutterSecureStorage();
+}
+
