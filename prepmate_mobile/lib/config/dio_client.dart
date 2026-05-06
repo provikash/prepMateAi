998ae98e619b783464114e3fa0a96d5499144776
Vikash <vkchaurasiyatrade@gmail.com>
2026-05-06 23:51:13 +0530

    update

--- prepmate_mobile/lib/config/dio_client.dart
+++ prepmate_mobile/lib/config/dio_client.dart
@@ -20,11 +20,8 @@ final secureStorageProvider = Provider<FlutterSecureStorage>(
 ///   3. [onError]    — on 401, attempt one token refresh then retry;
 ///                     if that fails, trigger forced logout.
 final dioProvider = Provider<Dio>((ref) {
-  // ⚠️  Configure your backend URL here:
-  // For Android Emulator (default): http://10.0.2.2:8000/api/v1/
-  // For Physical Device: http://<YOUR_MACHINE_IP>:8000/api/v1/
-  // Example: http://192.168.1.100:8000/api/v1/
-  const baseUrl = 'http://10.24.117.1:8000/api/v1/';
+  
+  const baseUrl = 'http://10.203.119.93:8000//api/v1/';
   
   final dio = Dio(
     BaseOptions(
